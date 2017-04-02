91ad0ea90d995b55bc4582c452e22d9b6a0deb00
HaiderMalik <haidermalik504@gmail.com>
2017-04-02 18:26:39 +0500

    Created User Controller

---
+++ api/controllers/UserController.js
@@ -0,0 +1,21 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  }
+};
+
