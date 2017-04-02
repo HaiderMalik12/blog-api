11674d0dd8578cb3c35b880356da398a445df24c
HaiderMalik <haidermalik504@gmail.com>
2017-04-02 17:45:01 +0500

    User models created

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/**
+ * User.js
+ *
+ * @description :: Each user can create many posts
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+ tableName:"users",
+
+  attributes: {
+
+    email : { type: 'string',required: true },
+
+    password : { type: 'string', required: true },
+
+    first_name : { type: 'string' },
+
+    last_name : { type: 'string' }
+  }
+};
+
